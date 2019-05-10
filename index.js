a404d15fd5897efa94621d4e116be6658ddf832d
madrigal415 <dsmadrigal@gmail.com>
2019-05-10 13:23:01 -0700

    adding new css style sheet

--- index.js
+++ index.js
@@ -3,60 +3,82 @@ const APIkey = "QKBL-P2TX-9BBT-DWE9";
 const timeEstEB = document.querySelector("#time-estimate-eb");
 const timeEstSF = document.querySelector("#time-estimate-sf");
 
-fetch('https://api.bart.gov/api/etd.aspx?cmd=etd&orig=24th&key=MW9S-E7SL-26DU-VV8V&json=y')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        const etds = myJson.root.station[0].etd;
-        console.table(etds);
+fetch(
+  "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=24th&key=MW9S-E7SL-26DU-VV8V&json=y"
+)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    const etds = myJson.root.station[0].etd;
+    console.table(etds);
 
-        let timesEB = '<h2>Next train departing 24th Street to EAST BAY: </h2><ul class="destinations">';
+    let timesEB =
+      '<h2>Next train departing 24th Street to EAST BAY: </h2><ul class="destinations">';
 
-        let timesSF = '<h2>Next train departing 24th Street to SF: </h2><ul class="destinations">';
+    let timesSF =
+      '<h2>Next train departing 24th Street to SF: </h2><ul class="destinations">';
 
-        function isEastBay(val) {
-            const ebStations = ['ANTC', 'DUBL', 'RICH', 'WARM'];
-            return ebStations.includes(val);
-        }
+    function isEastBay(val) {
+      const ebStations = ["ANTC", "DUBL", "RICH", "WARM"];
+      return ebStations.includes(val);
+    }
 
-        function isSF(val) {
-            const sfStations = ['DALY', 'MLBR', 'SFIA'];
-            return sfStations.includes(val);
-        }
+    function isSF(val) {
+      const sfStations = ["DALY", "MLBR", "SFIA"];
+      return sfStations.includes(val);
+    }
 
-        // filter for only East Bay bound trains 
-        let etdsEB = etds.filter(x => isEastBay(x.abbreviation));
+    // filter for only East Bay bound trains
+    let etdsEB = etds.filter(x => isEastBay(x.abbreviation));
 
-
-        // adding the destinations and times to the page for EB direction
-        etdsEB.forEach(element => {
-            timesEB += `<li>${element.abbreviation}</li><ul class="times">`;
-            element.estimate.forEach(x => {
-                timesEB += `<li>${x.minutes}, &nbsp</li>`;
-            });
-            timesEB += '</ul>';
-        });
-
-        timesEB += "</ul>";
-        console.log(timesEB);
-        timeEstEB.innerHTML = timesEB;
-
-        // filter for only SF bound trains 
-        let etdsSF = etds.filter(x => isSF(x.abbreviation));
-
-        // adding the destinations and times to the page for SF direction
-        etdsSF.forEach(element => {
-            timesSF += `<li>${element.abbreviation}</li><ul class="times">`;
-            element.estimate.forEach(x => {
-                timesSF += `<li>${x.minutes}, &nbsp</li>`;
-            });
-            timesSF += '</ul>';
-        });
-
-        timesSF += "</ul>";
-
-
-        timeEstSF.innerHTML = timesSF;
-
+    // adding the destinations and times to the page for EB direction
+    etdsEB.forEach(element => {
+      timesEB += `<li>${element.abbreviation}</li><ul class="times">`;
+      element.estimate.forEach(x => {
+        timesEB += `<li>${x.minutes}, &nbsp</li>`;
+      });
+      timesEB += "</ul>";
     });
+
+    timesEB += "</ul>";
+    console.log(timesEB);
+    timeEstEB.innerHTML = timesEB;
+
+    // filter for only SF bound trains
+    let etdsSF = etds.filter(x => isSF(x.abbreviation));
+
+    // adding the destinations and times to the page for SF direction
+    etdsSF.forEach(element => {
+      timesSF += `<li>${element.abbreviation}</li><ul class="times">`;
+      element.estimate.forEach(x => {
+        timesSF += `<li>${x.minutes}, &nbsp</li>`;
+      });
+      timesSF += "</ul>";
+    });
+
+    timesSF += "</ul>";
+
+    timeEstSF.innerHTML = timesSF;
+  });
+
+// get date and time
+const x = function() {
+  let today = new Date();
+  let hour = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+  let ampm = today.getHours() < 12 ? "am" : "pm";
+
+  let minutes =
+    today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+  let seconds =
+    today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+
+  let time = `${hour}:${minutes}:${seconds} ${ampm}`;
+  return time;
+};
+
+document.getElementById("time").innerHTML = x();
+
+setInterval(function() {
+  document.getElementById("time").innerHTML = x();
+}, 10000);
